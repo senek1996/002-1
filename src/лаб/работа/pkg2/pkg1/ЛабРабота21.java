24e41c5b4f0d253904b407226dc582bc686482f3
unknown <bai1541b@k3-426-10.edu.ugrasu>
2016-02-22 12:13:41 +0500

    123

---
+++ src/лаб/работа/pkg2/pkg1/ЛабРабота21.java
@@ -0,0 +1,16 @@
+/*
+ * Эта прога выводит четные числа [1..100]
+ */
+package лаб.работа.pkg2.pkg1;
+
+
+public class ЛабРабота21 {
+
+
+    public static void main(String[] args) {
+        for (int i=2;i<=100;i=i+2) {
+            System.out.print(i+" ");
+        }
+    }
+    
+}
